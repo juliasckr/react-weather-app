36952c9eb91cd0fa7693be133790cc0ae40c27b8
juliasckr <jule.s@live.de>
2021-04-21 11:19:12 +0200

    Updates

---
+++ precache-manifest.381b1f21183403086dbcc4edd1500b67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeb2df4df1a8d0e687d1d3817d2ca199",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c9fa35a528069f2e6670",
+    "url": "./static/css/main.5038533c.chunk.css"
+  },
+  {
+    "revision": "df1e758f55f3dbc68706",
+    "url": "./static/js/2.31062568.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.31062568.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9fa35a528069f2e6670",
+    "url": "./static/js/main.f2eace11.chunk.js"
+  },
+  {
+    "revision": "8cdb25f3ffb41440c4d8",
+    "url": "./static/js/runtime-main.e6c4d886.js"
+  }
+]);
